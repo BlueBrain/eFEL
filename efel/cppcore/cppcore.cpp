8a0c4de52c6982949ce586fd5813c8bed37baf91
Werner Van Geit <werner.vangeit@epfl.ch>
2015-05-06 16:48:19 +0200

    Added cppcore wrapper and setup.py

---
+++ efel/cppcore/cppcore.cpp
@@ -0,0 +1,201 @@
+/* Copyright (c) 2015, EPFL/Blue Brain Project                              
+ *                                                                               
+ * This file is part of eFEL <https://github.com/BlueBrain/eFEL>               
+ *                                                                               
+ * This library is free software; you can redistribute it and/or modify it under 
+ * the terms of the GNU Lesser General Public License version 3.0 as published   
+ * by the Free Software Foundation.                                              
+ *                                                                               
+ * This library is distributed in the hope that it will be useful, but WITHOUT   
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more 
+ * details.                                                                      
+ *                                                                               
+ * You should have received a copy of the GNU Lesser General Public License      
+ * along with this library; if not, write to the Free Software Foundation, Inc., 
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.                   
+ */                                     
+
+/*
+ * =============================================================================
+ *
+ *       Filename:  CppCore.cpp
+ *
+ *    Description:  Python wrapper file, to be compiled as shared library
+ *
+ *        Version:  1.0
+ *        Created:  05/06/2015
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Werner Van Geit,
+ *        Company:  BBP
+ *
+ * =============================================================================
+ */
+
+#include <cfeature.h>
+#include <Feature.h>
+#include <Python.h>
+
+using namespace std;
+
+extern cFeature* pFeature;
+
+
+static PyObject* CppCoreInitialize(PyObject * self, PyObject * args) {
+
+    char * depfilename, * outfilename;
+    if (!PyArg_ParseTuple(args, "ss", &depfilename, &outfilename)) {
+        return NULL;
+    }
+
+    Initialize(depfilename, outfilename);
+    return Py_BuildValue("");
+}
+
+
+static vector<int> PyList_to_vectorint(PyObject *input) {
+    vector<int> result_vector;
+    int list_size;
+    int index;
+      
+    list_size = PyList_Size(input);
+    for (index = 0; index < list_size; index++) {
+        result_vector.push_back(PyInt_AsLong(PyList_GetItem(input, index)));
+    } 
+    return result_vector;
+}
+
+void PyList_from_vectorint(vector<int> input, PyObject *output) {
+    int vector_size;
+    int index;
+
+    vector_size = input.size();
+    for (index = 0; index < vector_size; index++) {
+        PyList_Append(output, Py_BuildValue("i", input[index]));
+    } 
+}
+
+static vector<double> PyList_to_vectordouble(PyObject *input) {
+    vector<double> result_vector;
+    int list_size;
+    int index;
+      
+    list_size = PyList_Size(input);
+    for (index = 0; index < list_size; index++) {
+        result_vector.push_back(PyFloat_AsDouble(PyList_GetItem(input, index)));
+    } 
+    return result_vector;
+}
+
+void PyList_from_vectordouble(vector<double> input, PyObject *output) {
+    int vector_size;
+    int index;
+
+    vector_size = input.size();
+    for (index = 0; index < vector_size; index++) {
+        PyList_Append(output, Py_BuildValue("f", input[index]));
+    } 
+}
+
+static PyObject* setfeatureint(PyObject * self, PyObject * args) {
+  char * feature_name;
+  PyObject * py_values;
+  vector<int> values; 
+  int return_value;
+  if (!PyArg_ParseTuple(args, "sO!", &feature_name, &PyList_Type, &py_values)) {
+          return NULL;
+  }
+  
+  values = PyList_to_vectorint(py_values);
+  return_value = pFeature->setFeatureInt(string(feature_name), values);
+  
+  return Py_BuildValue("i", return_value); 
+}
+
+static PyObject* getfeatureint(PyObject * self, PyObject * args) {
+  char * feature_name;
+  PyObject * py_values;
+  vector<int> values; 
+  int return_value;
+  if (!PyArg_ParseTuple(args, "sO!", &feature_name, &PyList_Type, &py_values)) {
+          return NULL;
+  }
+ 
+ 
+  return_value = pFeature->getFeatureInt(string(feature_name), values);
+  PyList_from_vectorint(values, py_values);
+  
+  return Py_BuildValue("i", return_value); 
+}
+
+static PyObject* setfeaturedouble(PyObject * self, PyObject * args) {
+  char * feature_name;
+  PyObject * py_values;
+  vector<double> values; 
+  int return_value;
+  if (!PyArg_ParseTuple(args, "sO!", &feature_name, &PyList_Type, &py_values)) {
+          return NULL;
+  }
+  
+  values = PyList_to_vectordouble(py_values);
+  return_value = pFeature->setFeatureDouble(string(feature_name), values);
+  
+  return Py_BuildValue("f", return_value); 
+}
+
+static PyObject* getfeaturedouble(PyObject * self, PyObject * args) {
+  char * feature_name;
+  PyObject * py_values;
+  vector<double> values; 
+  int return_value;
+  if (!PyArg_ParseTuple(args, "sO!", &feature_name, &PyList_Type, &py_values)) {
+          return NULL;
+  }
+ 
+  return_value = pFeature->getFeatureDouble(string(feature_name), values);
+  PyList_from_vectordouble(values, py_values);
+
+  return Py_BuildValue("i", return_value); 
+}
+
+static PyObject* featuretype(PyObject * self, PyObject * args) {
+  char * feature_name;
+  string feature_type;
+  
+  if (!PyArg_ParseTuple(args, "s", &feature_name)) {
+          return NULL;
+  }
+  
+  feature_type = pFeature->featuretype(string(feature_name));
+
+  return Py_BuildValue("s", feature_type.c_str());
+}
+
+static PyObject* getgerrorstr(PyObject * self, PyObject * args) {
+    return Py_BuildValue("s", pFeature->getGError().c_str());
+}
+
+
+static PyMethodDef CppCoreMethods[] = {                                      
+            {"Initialize",  CppCoreInitialize, METH_VARARGS,                      
+                               "Initialise CppCore."},                               
+            {"setFeatureInt",  setfeatureint, METH_VARARGS,                      
+                               "Set a integer feature."},                               
+            {"getFeatureInt",  getfeatureint, METH_VARARGS,                      
+                               "Set a integer feature."},                               
+            {"setFeatureDouble",  setfeaturedouble, METH_VARARGS,                      
+                               "Set a double feature."},                               
+            {"getFeatureDouble",  getfeaturedouble, METH_VARARGS,                      
+                               "Get a double feature."},                               
+            {"featuretype",  featuretype, METH_VARARGS,                      
+                               "Get the type of a feature"},                               
+            {"getgError",  getgerrorstr, METH_VARARGS,                      
+                               "Get CppCore error string"},                               
+                    {NULL, NULL, 0, NULL}        /* Sentinel */                              
+};                                                                               
+                                                                                 
+PyMODINIT_FUNC initcppcore(void) {                                           
+        (void) Py_InitModule("cppcore", CppCoreMethods);                     
+}           
---
+++ setup.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+""" eFEL setup """
+
+"""
+Copyright (c) 2015, EPFL/Blue Brain Project
+
+ This file is part of eFEL <https://github.com/BlueBrain/eFEL>
+
+ This library is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License version 3.0 as published
+ by the Free Software Foundation.
+
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this library; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension  # pylint: disable=E0611,F0401
+
+import pip.req
+import os
+
+VERSION = "2.2.0"
+
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def parse_reqs(reqs_file):
+    ''' parse the requirements '''
+    install_reqs = pip.req.parse_requirements(
+        reqs_file,
+        session=False)
+    return [str(ir.req) for ir in install_reqs]
+
+REQS = parse_reqs(os.path.join(BASEDIR, "requirements.txt"))
+
+EXTRA_REQS_PREFIX = 'requirements_'
+EXTRA_REQS = {}
+for file_name in os.listdir(BASEDIR):
+    if not file_name.startswith(EXTRA_REQS_PREFIX):
+        continue
+    base_name = os.path.basename(file_name)
+    (extra, _) = os.path.splitext(base_name)
+    extra = extra[len(EXTRA_REQS_PREFIX):]
+    EXTRA_REQS[extra] = parse_reqs(file_name)
+
+cppcore_dir = os.path.join('efel', 'cppcore')
+cppcore_sources = ['cppcore.cpp',
+                   'Utils.cpp',
+                   'LibV1.cpp',
+                   'LibV2.cpp',
+                   'LibV3.cpp',
+                   'LibV4.cpp',
+                   'LibV5.cpp',
+                   'FillFptrTable.cpp',
+                   'DependencyTree.cpp',
+                   'Feature.cpp',
+                   'cfeature.cpp',
+                   'mapoperations.cpp']
+cppcore_sources = [
+    os.path.join(
+        cppcore_dir,
+        filename) for filename in cppcore_sources]
+
+cppcore = Extension('cppwrapper',
+                    sources=cppcore_sources,
+                    include_dirs=['src'])
+setup(
+    name="efel",
+    version=VERSION,
+    install_requires=REQS,
+    packages=['efel'],
+    include_package_data=True,
+    author="Werner Van Geit",
+    author_email="werner.vangeit@epfl.ch",
+    description="Electrophysiological Feature Extraction Library",
+    license="BBP-internal-confidential",
+    keywords=('feature',
+              'extraction',
+              'electrophysiology',
+              'BlueBrainProject'),
+    url="http://bluebrain.epfl.ch",
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Environment :: Console',
+                 'License :: Proprietary',
+                 'Operating System :: POSIX',
+                 'Topic :: Scientific/Engineering',
+                 'Topic :: Utilities'],
+    package_data={'': ['VERSION.txt']},
+    ext_modules=[cppcore]
+)
