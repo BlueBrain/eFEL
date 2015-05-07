50a7d3d94a916ed5af8684b28ca1b6280b5d9aaf
Werner Van Geit <werner.vangeit@epfl.ch>
2015-05-07 16:19:05 +0200

    test_allfeatures running on normal features

--- efel/tests/test_allfeatures.py
+++ efel/tests/test_allfeatures.py
@@ -5,4 +5,38 @@ import nose.tools as nt
 
 def test_allfeatures():
     """allfeatures: Regression testing all features on a trace"""
-    pass
+    import efel
+    import numpy
+
+    all_featurenames = efel.getFeatureNames()
+
+    data = numpy.loadtxt('testdata/allfeatures/testdata.txt')
+
+    time = data[:, 0]
+    voltage = data[:, 1]
+
+    trace = {}
+
+    trace['T'] = time
+    trace['V'] = voltage
+    trace['stim_start'] = [700]
+    trace['stim_end'] = [2700]
+
+    import warnings
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        feature_values = efel.getFeatureValues([trace], all_featurenames)[0]
+
+    for feature_name in feature_values:
+        if feature_values[feature_name] is not None:
+            feature_values[feature_name] = list(
+                feature_values[feature_name])
+    import json
+    with open('testdata/allfeatures/expectedresults.json', 'r') \
+            as expected_json:
+        expected_results = json.load(expected_json)
+
+    for feature_name in all_featurenames:
+        nt.assert_equal(
+            feature_values[feature_name],
+            expected_results[feature_name])
