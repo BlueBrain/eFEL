a205cafa94bb9bdd25d8e6637c47ec69e9e2388f
Werner Van Geit <werner.vangeit@epfl.ch>
2015-05-11 19:54:49 +0200

    Fixed upload docs script

--- upload_docs.sh
+++ upload_docs.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-pip install . --upgrade
+python setup.py install
 
 SOURCES='doc/source efel doc/Makefile'
 
@@ -13,7 +13,7 @@ git checkout master ${SOURCES}
 git reset HEAD
 make -f doc/Makefile html
 mv -fv build/html/* ./
-rm -rf $(SOURCES) build
+rm -rf ${SOURCES} build
 git add -A
 git commit -m "Generated github docs for `git log master -1 --pretty=short \
 --abbrev-commit`" && git push origin gh-pages ; git checkout master
