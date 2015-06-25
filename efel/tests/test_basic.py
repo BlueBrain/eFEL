465dfadbe524317326bb12f2224a760f8e6baa9e
Werner Van Geit <werner.vangeit@epfl.ch>
2015-06-25 13:30:21 +0200

    Added unit tests and doc for steady_state_voltage

--- efel/tests/test_basic.py
+++ efel/tests/test_basic.py
@@ -491,6 +491,46 @@ def test_getFeatureNames():
     nt.assert_equal(efel.getFeatureNames(), expected_featurenames)
 
 
+def test_steady_state_voltage1():
+    """basic: steady_state_voltage 1"""
+
+    import efel
+    efel.reset()
+    import numpy
+
+    stim_start = 500.0
+    stim_end = 900.0
+
+    data = numpy.loadtxt('testdata/basic/mean_frequency_1.txt')
+
+    time = data[:, 0]
+    voltage = data[:, 1]
+
+    trace = {}
+
+    trace['T'] = time
+    trace['V'] = voltage
+    trace['stim_start'] = [stim_start]
+    trace['stim_end'] = [stim_end]
+
+    features = ['steady_state_voltage']
+
+    feature_values = \
+        efel.getFeatureValues(
+            [trace],
+            features)[0]
+
+    begin_time = stim_end
+    end_time = max(time)
+    steady_state_voltage = [
+        numpy.mean(voltage[numpy.where(
+            (time <= end_time) & (time > begin_time)
+        )])]
+
+    nt.assert_almost_equal(steady_state_voltage,
+                           feature_values['steady_state_voltage'])
+
+
 def test_steady_state_voltage_stimend():
     """basic: steady_state_voltage_stimend 1"""
 
