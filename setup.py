467dd165fe541ad2a5e44a102b1a2d3f370986a6
Joffrey Gonin <Joffrey.Gonin@gmail.com>
2017-04-04 15:16:55 +0200

    added scipy in  install_requires

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
     name="efel",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    install_requires=['numpy>=1.6', 'six', 'neo>=0.5.0'],
+    install_requires=['numpy>=1.6', 'six', 'neo>=0.5.0', 'scipy'],
     packages=['efel'],
     author="BlueBrain Project, EPFL",
     maintainer="Werner Van Geit",
