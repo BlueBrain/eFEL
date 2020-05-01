37a72086e12297219d598c70bbbb5c931e697ec5
Anil Tuncel <anil.tuncel@epfl.ch>
2020-05-01 15:58:49 +0200

    exclude impedance from allfeature_values tests

--- efel/tests/test_allfeatures.py
+++ efel/tests/test_allfeatures.py
@@ -119,7 +119,7 @@ def get_allfeature_values():
     for feature_name in db_featurenames:
         soma_featurenames.remove(feature_name)
 
-    soma_featurenames = [x for x in soma_featurenames if x != 'current']
+    soma_featurenames = [x for x in soma_featurenames if x not in ['current', 'impedance']]
 
     import warnings
     with warnings.catch_warnings():
