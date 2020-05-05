3ed5f29516ab16681b0786ca14c84d282213d69a
Anil Tuncel <anil.tuncel@epfl.ch>
2020-05-05 11:20:00 +0200

    remove unnecessary brackets

--- efel/pyfeatures/pyfeatures.py
+++ efel/pyfeatures/pyfeatures.py
@@ -65,7 +65,7 @@ def impedance():
         fft_volt = numpy.fft.fft(normalized_voltage * 1e3)
         fft_cur = numpy.fft.fft(normalized_current * 1e12)
         freq = numpy.fft.fftfreq(len(normalized_voltage), d=0.0001)
-        Z = ((fft_volt) / (fft_cur))
+        Z = fft_volt / fft_cur
         norm_Z = abs(Z[1:150]) / max(abs(Z[1:150]))
         smooth_Z = gaussian_filter1d(norm_Z, 10)
         ind_max = numpy.argmax(smooth_Z)
