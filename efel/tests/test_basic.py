074dd1a546ed1a8b005a847a6fdec03b984868c8
Anil Tuncel <anil.tuncel@epfl.ch>
2020-06-03 11:42:11 +0200

    voltagebase_median test increase precision places to check

--- efel/tests/test_basic.py
+++ efel/tests/test_basic.py
@@ -932,7 +932,7 @@ def test_voltagebase_median():
         (interp_time >= 0.9 * stim_start) & (interp_time <= stim_start))])
 
     nt.assert_almost_equal(voltage_base, feature_values[0]['voltage_base'][0],
-                           places=5)
+                           places=8)
 
 
 def test_currentbase():
