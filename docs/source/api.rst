cc4f705be0e06d2fd9b26a365a6dde41f6c8306c
Anil Tuncel <anil.tuncel@epfl.ch>
2024-01-08 14:46:37 +0100

    docs: add multitrace and validation modules to API

--- docs/source/api.rst
+++ docs/source/api.rst
@@ -15,6 +15,8 @@ Submodules
 
    api
    io
+   pyfeatures.multitrace
    pyfeatures.pyfeatures
    units
    settings
+   validation
--- efel/validation.py
+++ efel/validation.py
@@ -3,7 +3,7 @@ import efel
 
 
 def check_ais_initiation(soma_trace: dict, ais_trace: dict) -> bool:
-    """Checks the initiation of action potential in AIS wrt. soma."""
+    """Checks the initiation of action potential in AIS with respect to soma."""
     f_values = efel.getFeatureValues([soma_trace, ais_trace], ["AP_begin_time"])
     soma_ap_begin_time = f_values[0]["AP_begin_time"]
     ais_ap_begin_time = f_values[1]["AP_begin_time"]
