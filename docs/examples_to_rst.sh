2134768f608632a7833f3cf3f314fe0bd1470a51
Aurélien Jaquier <72930209+AurelienJaquier@users.noreply.github.com>
2024-04-25 14:27:13 +0200

    Fixing Readthedocs (#381)

--- docs/examples_to_rst.sh
+++ docs/examples_to_rst.sh
@@ -1,6 +1,17 @@
-jupyter nbconvert --to rst ../examples/sonata-network/sonata-network.ipynb
-jupyter nbconvert --to rst ../examples/nmc-portal/L5TTPC2.ipynb
-cp ../examples/sonata-network/sonata-network.rst source/
-cp -r ../examples/sonata-network/sonata-network_files source/
-cp ../examples/nmc-portal/L5TTPC2.rst source/nmc-portal.rst
-cp -r ../examples/nmc-portal/L5TTPC2_files source/
+# to call from root
+
+# clean up
+rm docs/source/sonata-network.rst
+rm -rf docs/source/sonata-network_files
+rm docs/source/nmc-portal.rst
+rm -rf docs/source/L5TTPC2_files
+
+# convert
+jupyter nbconvert --to rst examples/sonata-network/sonata-network.ipynb
+jupyter nbconvert --to rst examples/nmc-portal/L5TTPC2.ipynb
+
+# move
+mv examples/sonata-network/sonata-network.rst docs/source/
+mv examples/sonata-network/sonata-network_files docs/source/
+mv examples/nmc-portal/L5TTPC2.rst docs/source/nmc-portal.rst
+mv examples/nmc-portal/L5TTPC2_files docs/source/
